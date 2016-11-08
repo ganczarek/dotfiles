a8001ff4c876c42af6e21404dfcaa89604e18f82
Rafal Ganczarek <rafal@ganczarek.info>
2016-11-08 22:56:14 +0000

    Install and configure oh-my-zsh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,28 @@
 #!/usr/bin/env bash
+
+DOTFILESDIR="`pwd`/`dirname $0`"
+
+link_dot_file() {
+  SOURCE=${DOTFILESDIR}/$1
+  DEST=$2
+
+  if [[ ! -L "$DEST" ]]; then
+    if [[ -e "$DEST" ]]; then
+        echo "Backing up $DEST under ${DEST}_backup"
+        mv ${DEST} ${DEST}_backup
+    fi
+
+    echo "Create symbolic link tp ${DEST} FROM ${SOURCE}"
+    ln -s ${SOURCE} ${DEST}
+  fi
+}
+
+# Install oh-my-zsh (https://github.com/robbyrussell/oh-my-zsh)
+if [ ! -n "$ZSH" ]; then
+  ZSH=~/.oh-my-zsh
+fi
+if [ ! -d "$ZSH" ]; then
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+fi
+
+link_dot_file zsh/zshrc ~/.zshrc
