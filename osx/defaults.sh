199e7a635ca689a2ea2ca48287955301d4df2779
Rafal Ganczarek <git@ganczarek.info>
2017-02-07 14:10:45 +0000

    Setup key repeat delays on Mac OS

--- osx/defaults.sh
+++ osx/defaults.sh
@@ -1,2 +1,6 @@
 # disable Character Accents Popup and enable Key Repeat when holding down a key
 defaults write -g ApplePressAndHoldEnabled -bool false
+
+# you could use Karabiner as well
+defaults write -g InitialKeyRepeat -float 10.0  # 166ms
+defaults write -g KeyRepeat -float 1.2          # 20ms
