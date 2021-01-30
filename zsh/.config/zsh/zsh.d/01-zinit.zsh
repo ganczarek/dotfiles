9a55c3a60f18c21ec99ec4e22cc1efb19667242d
Rafal Ganczarek <github@ganczarek.info>
2021-01-30 15:53:26 +0000

    Enabled zsh-autosuggestions

--- zsh/.config/zsh/zsh.d/01-zinit.zsh
+++ zsh/.config/zsh/zsh.d/01-zinit.zsh
@@ -4,3 +4,10 @@ export ZINIT[BIN_DIR]="$ZINIT[HOME_DIR]/bin"
 export ZINIT[ZCOMPDUMP_PATH]="$ZINIT[HOME_DIR]/zcompdump"
 
 source $ZINIT[BIN_DIR]/zinit.zsh
+
+# make suggestions visible with solarized-dark theme
+ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=#657b83"
+ZSH_AUTOSUGGEST_STRATEGY=(history completion)
+ZSH_AUTOSUGGEST_USE_ASYNC=1
+zinit wait lucid for atload"_zsh_autosuggest_start" zsh-users/zsh-autosuggestions
+
