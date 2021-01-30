c0b4797ef1693c641246b81767c067c88515f161
Rafal Ganczarek <github@ganczarek.info>
2021-01-30 16:16:21 +0000

    Use exa instead of GNU ls

--- zsh/.config/zsh/zsh.d/aliases.zsh
+++ zsh/.config/zsh/zsh.d/aliases.zsh
@@ -1,2 +1,9 @@
 # tmux devs refuse to support XDG, so use an alias as a workaround (see https://github.com/tmux/tmux/issues/142)
 alias tmux='tmux -f "$XDG_CONFIG_HOME"/tmux/tmux.conf'
+
+if command -v exa >/dev/null 2>&1; then
+  alias gnuls='/bin/ls'
+  alias la='exa -a'
+  alias ll='exa -al'
+  alias ls='exa'
+fi
