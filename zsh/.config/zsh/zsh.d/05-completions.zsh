aba1bd805b1c44df3f7c8be2178df94180ce8ed6
Rafal Ganczarek <16733667+ganczarek@users.noreply.github.com>
2021-07-07 08:38:45 +0100

    Setup ZSH completions for hosts in SSH config

--- zsh/.config/zsh/zsh.d/05-completions.zsh
+++ zsh/.config/zsh/zsh.d/05-completions.zsh
@@ -13,4 +13,17 @@ ZSH_AUTOSUGGEST_STRATEGY=(history completion)
 ZSH_AUTOSUGGEST_USE_ASYNC=1
 zinit wait lucid for atload"_zsh_autosuggest_start" zsh-users/zsh-autosuggestions
 
+# based on https://serverfault.com/questions/170346/how-to-edit-command-completion-for-ssh-on-zsh/170481#170481
+h=()
+for file in ~/.ssh/config*; do
+  h=($h ${${${(@M)${(f)"$(cat $file)"}:#Host *}#Host }:#*[*?]*})
+done
+if [[ -r ~/.ssh/known_hosts ]]; then
+  h=($h ${${${(f)"$(cat ~/.ssh/known_hosts{,2} || true)"}%%\ *}%%,*}) 2>/dev/null
+fi
+if [[ $#h -gt 0 ]]; then
+  zstyle ':completion:*:ssh:*' hosts $h
+  zstyle ':completion:*:slogin:*' hosts $h
+fi
+
 zinit wait lucid for zdharma/fast-syntax-highlighting
