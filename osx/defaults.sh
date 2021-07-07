9c8b3ce549f66975d50a6dd8bfacd7db66f1a2fa
Rafal Ganczarek <16733667+ganczarek@users.noreply.github.com>
2021-07-07 12:58:34 +0100

    Added iTerm2 config

--- osx/defaults.sh
+++ osx/defaults.sh
@@ -8,3 +8,7 @@ defaults write -g InitialKeyRepeat -float 10.0  # 166ms
 defaults write -g KeyRepeat -float 1.2          # 20ms
 # Use all Fn keys as standard function keys
 defaults write -g com.apple.keyboard.fnState -bool true
+
+# iTerm2 config
+defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "$HOME/.config/iterm2"
+defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
