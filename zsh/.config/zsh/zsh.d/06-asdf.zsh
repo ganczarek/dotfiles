c78ee80fc98ae99940b4a06283e7f207fa59398d
Rafal Ganczarek <16733667+ganczarek@users.noreply.github.com>
2022-04-02 10:18:08 +0100

    Fixed asdf config

--- zsh/.config/zsh/zsh.d/06-asdf.zsh
+++ zsh/.config/zsh/zsh.d/06-asdf.zsh
@@ -1,18 +1,22 @@
 if [[ -d /opt/asdf-vm ]]; then
-  . /opt/asdf-vm/asdf.sh
+    export ASDF_DIR=/opt/asdf-vm
+    export ASDF_CONFIG_FILE=$XDG_CONFIG_HOME/asdf/asdfrc
+    export ASDF_DATA_DIR=$XDG_DATA_HOME/asdf
+
+    source /opt/asdf-vm/asdf.sh
+
+    zinit wait lucid for OMZP::asdf
+
+    ASDF_PLUGINS=(
+      java
+      python
+    )
+    ASDF_PLUGINS_ALREADY_INSTALLED=$(asdf plugin list)
+
+    for PLUGIN in $ASDF_PLUGINS; do
+      if ! (($ASDF_PLUGINS_ALREADY_INSTALLED[(Ie)$PLUGIN])); then
+        echo "Adding plugin '$PLUGIN' to asdf"
+        asdf plugin-add $PLUGIN
+      fi
+    done
 fi
-
-zinit wait lucid for OMZP::asdf
-
-ASDF_PLUGINS=(
-  java
-  python
-)
-ASDF_PLUGINS_ALREADY_INSTALLED=$(asdf plugin list)
-
-for PLUGIN in $ASDF_PLUGINS; do
-  if ! (($ASDF_PLUGINS_ALREADY_INSTALLED[(Ie)$PLUGIN])); then
-    echo "Adding plugin '$PLUGIN' to asdf"
-    asdf plugin-add $PLUGIN
-  fi
-done
