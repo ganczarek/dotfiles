f76d1085e1d77e009772cb84867e2903e8933d9d
Rafal Ganczarek <16733667+ganczarek@users.noreply.github.com>
2022-10-07 16:25:41 +0100

    Increased shell history size to 1e7

--- zsh/.config/zsh/zsh.d/04-history.zsh
+++ zsh/.config/zsh/zsh.d/04-history.zsh
@@ -3,7 +3,7 @@ zinit snippet OMZL::history.zsh
 zinit wait lucid for zdharma/history-search-multi-word
 
 export HIST_STAMPS="yyyy-mm-dd"
-export HISTSIZE=100000
-export HISTFILESIZE=100000
+export HISTSIZE=10000000
+export HISTFILESIZE=10000000
 
 setopt hist_reduce_blanks # Remove superfluous blanks before recording entry.
