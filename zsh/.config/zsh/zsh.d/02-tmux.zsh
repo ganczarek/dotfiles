cb240975d5c50fe2fdec5d619ba82bebb2c62084
Rafal Ganczarek <16733667+ganczarek@users.noreply.github.com>
2022-11-13 15:31:01 +0000

    Start tmux session in i3 terminal

--- zsh/.config/zsh/zsh.d/02-tmux.zsh
+++ zsh/.config/zsh/zsh.d/02-tmux.zsh
@@ -1,7 +1,20 @@
 # check if tmux is installed
 if command -v tmux >/dev/null 2>&1; then
-   # Don't use tmux inside emacs (dumb or empty $TERM') or IntelliJ (JetBrains term emulator)
-   if [[ $TERM != "dumb" && $TERM != "" && ! $TERM =~ screen && $XDG_CURRENT_DESKTOP != i3 && ! $TERMINAL_EMULATOR =~ JetBrains ]]; then
-      exec tmux new-session -A -s main
-   fi
+  # Don't use tmux inside emacs
+  if [[ $TERM == "dumb" || $TERM == "" ]]; then
+    :
+  # don't use tmux inside JetBrains term emulator
+  elif [[ $TERMINAL_EMULATOR =~ JetBrains ]]; then
+    :
+  # don't create nested tmux sessions
+  elif [[ $TERM =~ screen ]]; then
+    :
+  else
+    # create new tmux session for each window in i3
+    if [[ $XDG_CURRENT_DESKTOP == i3 && -z $TMUX_SESSION ]]; then
+      exec tmux new-session
+    else
+      exec tmux new-session -A -s "${TMUX_SESSION:-main}"
+    fi
+  fi
 fi
