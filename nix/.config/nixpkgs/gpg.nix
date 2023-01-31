d5ef63dae17fda08cd66cc036bc04798a4efa4a1
Rafal Ganczarek <16733667+ganczarek@users.noreply.github.com>
2023-01-31 23:19:18 +0000

    Added gpg config to home manager

---
+++ nix/.config/nixpkgs/gpg.nix
@@ -0,0 +1,26 @@
+{ pkgs, config, ... }:
+
+{
+  programs.gpg = {
+    enable = true;
+    package = pkgs.hello;
+    homedir = "${config.xdg.configHome}/gnupg";
+
+    # content of gpg.conf
+    settings = {
+      keyserver = "hkp://keys.openpgp.net";
+    };
+  };
+
+  services.gpg-agent = {
+    enable = true;
+    defaultCacheTtl = 14400; # 4 hours
+    maxCacheTtl = 28800; # 8 hours
+    enableScDaemon = false;
+    enableSshSupport = true;
+    pinentryFlavor = null;
+    extraConfig = ''
+      pinentry-program /usr/bin/pinentry
+    '';
+  };
+}
