457d86d8abffcdaaac2d9ab66d02b4e858925ca9
Rafal Ganczarek <16733667+ganczarek@users.noreply.github.com>
2023-04-02 11:33:38 +0100

    Allowed to delete files in tig status window

--- nix/.config/nixpkgs/tig.nix
+++ nix/.config/nixpkgs/tig.nix
@@ -4,6 +4,7 @@
   home.file."${config.xdg.configHome}/tig/config".text = ''
     # allows to amend commit in status view with 'a' binding
     bind status a !?git commit --amend
+    bind status D !@?rm %(file)
 
     bind main = !git commit --fixup=%(commit)
     bind main <Ctrl-R> !git rebase --autosquash -i %(commit)
