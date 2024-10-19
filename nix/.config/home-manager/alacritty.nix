5de137e2d34cc80c32bed2421333332fc1eee071
Rafal Ganczarek <16733667+ganczarek@users.noreply.github.com>
2024-10-19 19:34:42 +0100

    Updated Alacritty config
    Removed unused settings (not needed after one of updates)

--- nix/.config/home-manager/alacritty.nix
+++ nix/.config/home-manager/alacritty.nix
@@ -5,13 +5,6 @@
   # don't install Alacritty (use pacman package)
   programs.alacritty.package = pkgs.hello;
   programs.alacritty.settings = {
-    # Available values:
-    # - full: borders and title bar
-    # - none: neither borders nor title bar
-    # - transparent: title bar, transparent background and title bar buttons
-    # - buttonless: title bar, transparent background, but no title bar buttons
-    window.declarations = "none";
     font.size = 10.0;
-    dynamic_title = "true";
   };
 }
